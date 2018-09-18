a34eee6021ea7d1078943f71e2f1422ced730fc0
leetkrew <rj@rjregalado.com>
2018-09-18 14:06:28 +0800

    added objecs in form

--- sampleWinformsProject_git/Form1.Designer.cs
+++ sampleWinformsProject_git/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAlert = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAlert
+            // 
+            this.btnAlert.Location = new System.Drawing.Point(255, 82);
+            this.btnAlert.Name = "btnAlert";
+            this.btnAlert.Size = new System.Drawing.Size(75, 23);
+            this.btnAlert.TabIndex = 0;
+            this.btnAlert.Text = "OK";
+            this.btnAlert.UseVisualStyleBackColor = true;
+            this.btnAlert.Click += new System.EventHandler(this.btnAlert_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(255, 56);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.Text = "txtName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnAlert);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAlert;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- sampleWinformsProject_git/Form1.cs
+++ sampleWinformsProject_git/Form1.cs
@@ -16,5 +16,10 @@ namespace sampleWinformsProject_git
         {
             InitializeComponent();
         }
+
+        private void btnAlert_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtName.Text);
+        }
     }
 }
